e132daf8da5922a5d516ca668c5a6bcdf4f7033f
kallehedengren-byte <kallehedengren@gmail.com>
2026-09-21 23:23:56 +0200

    Add SE3 spotelpris panel with today’s prices and hour chart
    Fetch elprisetjustnu.se SE3 data into data.json, show current/min/max/mean
    and cheapest hours near Idag, and bump SW cache to familjen-v4.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'familjen-v3';
+const CACHE = 'familjen-v4';
 const ASSETS = ['./', './index.html', './data.json', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
