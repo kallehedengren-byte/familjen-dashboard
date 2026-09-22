97d6d8ffe3add11be3aa26151f492cb55edd2192
kallehedengren-byte <kallehedengren-byte@users.noreply.github.com>
2026-09-22 07:10:47 +0200

    Fix broken renderToday that left dashboard stuck loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'familjen-v5';
+const CACHE = 'familjen-v6';
 const ASSETS = ['./', './index.html', './data.json', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
